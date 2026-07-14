d570804eb92c71902837ffda5c04a3d154893dcd
schmartmann <stefanhartmann@gmail.com>
2026-07-14 16:14:14 -0400

    make character list toggleable

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('here')
+const setSwitches = () => {
+    const characterDetailsArray = [...document.getElementsByClassName('character-block')];
+    characterDetailsArray.forEach(element => {
+        element.addEventListener('click', (e) => {
+            toggleHidden(e)
+        })
+    });
+}
+
+const toggleHidden = ({ target }) => {
+    target.nextElementSibling.classList.toggle('hidden')
+}
+
+
+
+window.onload = (event) => {
+    console.log('loaded');
+    setSwitches();
+}
