49ce3d0d63612f8a45437339d6c3383a2e11c382
schmartmann <stefanhartmann@gmail.com>
2026-07-16 17:21:31 -0400

    finish interactivity

--- main.js
+++ main.js
@@ -1,56 +1,67 @@
-const setSwitches = () => {
-    const popup = document.getElementById('character-detail-popup');
-    const characterBlocks = [...document.getElementsByClassName('character-block')];
+'use strict';
 
-    characterBlocks.forEach(element => {
-        element.addEventListener('click', (e) => {
-            toggleCharacterInfoBlock(e, popup)
-        })
-    });
+const handlePopupToggle = (event, popup) => {
+    const isClickedInteractives = event.target.classList.contains('character-button');
+    const clickIsOutsideOfPopup = !isClickedInteractives;
+    const isPopupOpen = !popup.classList.contains('hidden');
+
+    const shouldPopupOpen = isClickedInteractives && !isPopupOpen;
+    const shouldPopupClose = clickIsOutsideOfPopup && isPopupOpen;
+
+    if (shouldPopupOpen) {
+        populateCharacterInfoBlock(event.target, popup);
+        popup.classList.remove('hidden');
+    } else if (shouldPopupClose) {
+        popup.classList.add('hidden');
+    } else {
+        populateCharacterInfoBlock(event.target, popup);
+    }
 }
 
-const toggleCharacterInfoBlock = ({ target }, popup) => {
-    const characterName = target.innerText.toLowerCase();
-    populateCharacterInfoBlock(characterName, popup);
-    popup.classList.toggle('hidden');
-}
+const populateCharacterInfoBlock = (target, popup) => {
+    const name = target.innerText;
+    const characterData = characterDetails[name];
 
-const populateCharacterInfoBlock = (characterName, popup) => {
-    const characterData = characterDetails[characterName]
-
-    popup.children[0].innerText = characterName;
+    popup.children[0].innerText = name;
 
     popup.children[1].innerText = characterData.text;
 
     const imageTags = popup.children[2].children;
 
     for (let i = 0; i < imageTags.length; i ++) {
-        imageTags[i].src = `./assets/img/${characterName}/${i + 1}.jpeg`
+        imageTags[i].src = `./assets/img/${name}/${i + 1}.jpeg`
     }
 }
 
 const characterDetails = { 
-    'weaver': {
+    'Weaver': {
         text: 'This is where the text about Weaver goes.'
     },
-    'erin': {
+    'Erin': {
         text: 'This is where the text about Erin goes.'
     },
-    'greg': { 
+    'Greg': { 
         text: 'This is where the text about Greg goes.'
     },
-    'dean': {
+    'Dean': {
         text: 'This is where the text about Dean goes.'
     },
-    'aiden': {
+    'Aiden': {
         text: 'This is where the text about Aiden goes.'
     },
-    'scout': {
+    'Scout': {
         text: 'This is where the text about Scout goes.'
     }
 }
 
+const addInteractivity = () => {
+    const popup = document.getElementById('character-detail-popup');
+
+    document.addEventListener('click', (event) => {
+        handlePopupToggle(event, popup);
+    });
+}
+
 window.onload = (event) => {
-    console.log('loaded');
-    setSwitches();
+    addInteractivity();
 }
