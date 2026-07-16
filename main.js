c038b37f5b3c257f0d77e3e873950bdb623cd4a0
schmartmann <stefanhartmann@gmail.com>
2026-07-16 15:18:00 -0400

    open popup

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
 const setSwitches = () => {
-    const characterDetailsArray = [...document.getElementsByClassName('character-block')];
-    characterDetailsArray.forEach(element => {
+    const popup = document.getElementById('character-detail-popup');
+    const characterBlocks = [...document.getElementsByClassName('character-block')];
+
+    characterBlocks.forEach(element => {
         element.addEventListener('click', (e) => {
-            toggleHidden(e)
+            toggleCharacterInfoBlock(e, popup)
         })
     });
 }
 
-const toggleHidden = ({ target }) => {
-    const { nextElementSibling } = target;
-    if (nextElementSibling && nextElementSibling.className.includes('character-detail')) {
-        nextElementSibling.classList.toggle('hidden')
+const toggleCharacterInfoBlock = ({ target }, popup) => {
+    const characterName = target.innerText.toLowerCase();
+    populateCharacterInfoBlock(characterName, popup);
+    popup.classList.toggle('hidden');
+}
+
+const populateCharacterInfoBlock = (characterName, popup) => {
+    const characterData = characterDetails[characterName]
+
+    popup.children[0].innerText = characterName;
+
+    popup.children[1].innerText = characterData.text;
+
+    const imageTags = popup.children[2].children;
+
+    for (let i = 0; i < imageTags.length; i ++) {
+        imageTags[i].src = `./assets/img/${characterName}/${i + 1}.jpeg`
     }
 }
 
-
+const characterDetails = { 
+    'weaver': {
+        text: 'This is where the text about Weaver goes.'
+    },
+    'erin': {
+        text: 'This is where the text about Erin goes.'
+    },
+    'greg': { 
+        text: 'This is where the text about Greg goes.'
+    },
+    'dean': {
+        text: 'This is where the text about Dean goes.'
+    },
+    'aiden': {
+        text: 'This is where the text about Aiden goes.'
+    },
+    'scout': {
+        text: 'This is where the text about Scout goes.'
+    }
+}
 
 window.onload = (event) => {
     console.log('loaded');
